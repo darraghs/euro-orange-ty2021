411ae4b5d42783b7e85704da77e0e8409f199d4c
Darragh Sherwin <dsherwin@overstock.com>
2021-04-26 21:58:55 +0100

    Add layout

--- js/map.js
+++ js/map.js
@@ -3,6 +3,6 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
       center: { lat: 54.2767272, lng: -8.5064695 },
-    zoom: 17,
+    zoom: 9,
   });
 }
